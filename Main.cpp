fc0c465f1b939f306d5679c4ab21d0f27ab71f42
Theodore Margoles <thma4828@colorado.edu>
2020-07-15 11:29:48 -0600

    Add files via upload
    command line arg = tree depth added. appears to work up to depth 3!!!!

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,20 @@
 #include "Generator.h"
 #include "Position.h"
 #include "Piece.h"
+#include <cstdlib>
 
 #include <iostream>
 
 using namespace std;
 
 
-int main(){
+int main(int argc, char**argv){
+  if(argc < 2){
+    cout << "usage: ./build <int: tree depth>" << endl;
+    return -1;
+  }
   Position *pstart = new Position(T1);
   Generator G(pstart, BLACK);
-  G.build_tree(G.get_tree_root(), 0, BLACK, 1, false);
+  G.build_tree(G.get_tree_root(), 0, BLACK, atoi(argv[1]), false);
   return 0;
 }
