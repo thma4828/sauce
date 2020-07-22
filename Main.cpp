1e1155b3277c8eabf89d65e917952ee78ffe9fc2
thma4828 <thma4828@colorado.edu>
2020-07-22 07:28:55 -0600

    good signs

--- Main.cpp
+++ Main.cpp
@@ -19,5 +19,19 @@ int main(int argc, char**argv){
   G.build_tree(G.get_tree_root(), 0, BLACK, atoi(argv[1]), false);
   int ncount = G.count_tree_nodes(G.get_tree_root(), 0);
   cout << "Tree has: " << ncount << " nodes!" << endl;
+
+  cout << "--Entering Tree Exploration--" << endl;
+ 
+  Node *root = G.get_tree_root();
+  cout << "root has naive eval at: " << root->wb_ratio << " (wb ratio)" << endl;
+  int rnc = root->children.size();
+  cout << "root node has: " << rnc << " children." << endl;
+
+ 
+  for(int i=0; i<rnc; i++){
+  	Node *ch = root->children[i];
+	cout << "--->roots child #" << i << " naive valued at: " << ch->wb_ratio<< endl;
+	cout << "--->roots child #" << i << " has: " << ch->children.size() << " 	children" << endl;
+  }
   return 0;
 }
