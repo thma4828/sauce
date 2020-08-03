a89d4676a3673e784086cc834d354bcc847b995c
thma4828 <thma4828@colorado.edu>
2020-08-03 15:15:39 -0600

    queen should be complete.

--- Queen.cpp
+++ Queen.cpp
@@ -42,7 +42,91 @@ vector<Move> Queen::set_moves(){
 				moves.push_back(t1);
 				break; 
 			}
-		}	
+		}
+		X = x;
+		Y = y;
+		while(Y < 7){
+			Y++;
+			int sq = pos->the_board[X][Y];
+			if(sq == NULLCELL){
+				Move m1(X, Y, color, false, false, QUEEN, x, y);
+				moves.push_back(m1);
+			}else if(is_enemy_piece_type(sq)){
+				Move t1(X, Y, color, true, false, QUEEN, x, y);
+				moves.push_back(t1); 
+				break;
+			}
+		}
+		Y = y;
+		while(Y > 0){
+			Y--;
+			int sq = pos->the_board[X][Y]; 
+			if(sq == NULLCELL){
+				Move m1(X, Y, color, false, false, QUEEN, x, y);
+				moves.push_back(m1);
+			}else if(is_enemy_piece_type(sq)){
+				Move t1(X, Y, color, true, false, QUEEN, x, y);
+				moves.push_back(t1);
+				break;
+			}
+		}
+		X = x;
+		Y = y;
+		while(X < 7 && Y < 7){
+			X++; Y++;
+			int sq = pos->the_board[X][Y];
+			if(sq == NULLCELL){
+				Move m1(X, Y, color, false, false, QUEEN, x, y);
+				moves.push_back(m1);
+			}else if(is_enemy_piece_type(sq)){
+				Move t1(X, Y, color, true, false, QUEEN, x, y); 
+				moves.push_back(t1);
+				break;
+			}
+		}
+		X = x;
+		Y = y;
+		while(X < 7 && Y > 0){
+			X++; Y--;
+			int sq = pos->the_board[X][Y];
+			if(sq == NULLCELL){
+				Move m1(X, Y, color, false, false, QUEEN, x, y);
+				moves.push_back(m1);
+			}else if(is_enemy_piece_type(sq)){
+				Move t1(X, Y, color, true, false, QUEEN, x, y); 
+				moves.push_back(t1);
+				break;
+			}
+		}
+		X = x;	
+		Y = y; 
+		while(X > 0 && Y < 7){
+			X--; Y++;
+			int sq = pos->the_board[X][Y];
+			if(sq == NULLCELL){
+				Move m1(X, Y, color, false, false, QUEEN, x, y);
+				moves.push_back(m1);
+			}else if(is_enemy_piece_type(sq)){
+				Move t1(X, Y, color, true, false, QUEEN, x, y);
+				moves.push_back(t1);
+				break; 
+			}
+		}
+		X = x;
+		Y = y;
+		while(X > 0 && Y > 0){
+			X--; Y--;
+			int sq = pos->the_board[X][Y];
+			if(sq == NULLCELL){
+				Move m1(X, Y, color, false, false, QUEEN, x, y); 
+				moves.push_back(m1);
+			}else if(is_enemy_piece_type(sq)){
+				Move t1(X, Y, color, true, false, QUEEN, x, y);
+				moves.push_back(t1);
+				break;
+			}
+		
+		}
 	}
 
 	curr_moves = moves;
